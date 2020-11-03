b5f69d808367e07e18894ef288700fa4b6d4c918
amyJheng <f54469@gmail.com>
2020-11-03 17:08:38 +0800

    update

---
+++ Chap03/VueClock/VueClock/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app" style="margin:auto;width:30%">
+        <!--<Home msg="Hello world!" />-->
+        <digital-clock :blink="true" :displaySeconds="true" />
+    </div>
+</template>
+
+<script>
+    //import Home from './components/Home.vue';
+    import DigitalClock from "./components/Clock.vue";
+
+    export default {
+        name: 'app',
+        components: {
+            //Home
+            DigitalClock
+        }
+    };
+</script>
+
+<style>
+    .clock {
+        color: black;
+        padding: 0.3rem 0.6rem;
+        font-size: 3rem;
+        font-family: "Menlo", monospace;
+    }
+</style>
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Vue.js-Demo
+﻿# Vue.js-Demo
 Vue.js 感謝王寧疆老師用心指導
+
+使用Vue.js開發SPA網頁(Single Page Application)
+
+要做服務
+1.REST介面(功能URI)
+2.OData介面(URI 指令Order Fliter Expend)
